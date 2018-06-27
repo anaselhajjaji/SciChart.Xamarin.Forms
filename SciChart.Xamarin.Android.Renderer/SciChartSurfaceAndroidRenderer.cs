630dfe2d33e231355148b96477dabf3eee6cac63
Andrew Burnett-Thompson <andrew@scichart.com>
2018-06-27 18:44:18 +0100

    Debugging why ios/android charts dont appear (but WPF does)

--- SciChart.Xamarin.Android.Renderer/SciChartSurfaceAndroidRenderer.cs
+++ SciChart.Xamarin.Android.Renderer/SciChartSurfaceAndroidRenderer.cs
@@ -1,9 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 using Android.Content;
+using Java.Util;
 using SciChart.Charting.Visuals;
 using SciChart.Charting.Visuals.Axes;
 using SciChart.Data.Model;
+using SciChart.Drawing.Canvas;
+using SciChart.Drawing.OpenGL;
 using SciChart.Xamarin.Android.Renderer;
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.Android;
@@ -14,9 +18,16 @@ namespace SciChart.Xamarin.Android.Renderer
 {
     public class SciChartSurfaceAndroidRenderer : ViewRenderer<SciChart.Xamarin.Views.Visuals.SciChartSurface, SciChart.Charting.Visuals.SciChartSurface>
     {
-        protected SciChartSurfaceAndroidRenderer(Context context) : base(context)
+        private readonly Dictionary<string, Action<SciChart.Xamarin.Views.Visuals.SciChartSurface, SciChart.Charting.Visuals.SciChartSurface>> _propertyMap;
+        
+        public SciChartSurfaceAndroidRenderer(Context context) : base(context) 
         {
-        }
+            _propertyMap = new Dictionary<string, Action<Views.Visuals.SciChartSurface, SciChartSurface>>
+            {
+                {"Width", OnWidthChanged},
+                {"Height", OnHeightChanged}
+            };
+        }        
 
         protected override void OnElementChanged(ElementChangedEventArgs<Views.Visuals.SciChartSurface> e)
         {
@@ -24,6 +35,7 @@ namespace SciChart.Xamarin.Android.Renderer
             if (Control == null)
             {
                 this.SetNativeControl(new SciChartSurface(Context));
+                Control.RenderSurface = new RenderSurface(Context);
 
                 // Some dummy data 
                 Control.XAxes.Add(new NumericAxis(Context));
@@ -37,7 +49,22 @@ namespace SciChart.Xamarin.Android.Renderer
 
         protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
         {
+            if (_propertyMap.TryGetValue(e.PropertyName, out var handler))
+            {
+                var sciChartSurfaceView = sender as SciChart.Xamarin.Views.Visuals.SciChartSurface;
+                handler(sciChartSurfaceView, Control);
+            }
             base.OnElementPropertyChanged(sender, e);
+        }
+
+        private void OnWidthChanged(Views.Visuals.SciChartSurface scsView, SciChartSurface scsAndroid)
+        {
+            //scsAndroid.LayoutParameters.Width = (int) scsView.Width;
+        }
+
+        private void OnHeightChanged(Views.Visuals.SciChartSurface scsView, SciChartSurface scsAndroid)
+        {
+            //scsAndroid.LayoutParameters.Height = (int)scsView.Height;
         }
     }
 }
