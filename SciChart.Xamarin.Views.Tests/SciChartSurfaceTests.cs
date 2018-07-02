8cffc3f0d667a36abb511ffa6aeadce92d98ebfd
Andrew Burnett-Thompson <andrew@scichart.com>
2018-07-02 10:58:09 +0100

    Some simple tests.

---
+++ SciChart.Xamarin.Views.Tests/SciChartSurfaceTests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using SciChart.Xamarin.Views.Tests.Stubs;
+using SciChart.Xamarin.Views.Visuals;
+using SciChart.Xamarin.Views.Visuals.RenderableSeries;
+
+namespace SciChart.Xamarin.Views.Tests
+{
+    [TestFixture]
+    public class SciChartSurfaceTests
+    {
+        [Test]
+        public void WhenCreateSciChartSurface_ShouldPropagateBindingContext()
+        {
+            var scs = new SciChartSurface();
+            var rs = new StubRenderableSeries();
+            scs.BindingContext = "ABindingContext";
+
+            // Set binding context then series add
+            scs.RenderableSeries.Add(rs);            
+
+            Assert.That(rs.BindingContext, Is.EqualTo("ABindingContext"));
+        }
+
+        [Test]
+        public void WhenCreateSciChartSurface_AndAddSeries_ShouldPropagateBindingContext()
+        {
+            var scs = new SciChartSurface();
+            var rs = new StubRenderableSeries();
+            scs.RenderableSeries.Add(rs);
+
+            // Set binding context after series add 
+            scs.BindingContext = "ABindingContext";
+
+            Assert.That(rs.BindingContext, Is.EqualTo("ABindingContext"));
+        }
+
+        [Test]
+        public void WhenCreateSciChartSurface_AndNewRenderableSeriesCollection_ShouldPropagateBindingContext()
+        {
+            var scs = new SciChartSurface();
+            var rs = new StubRenderableSeries();
+            scs.BindingContext = "ABindingContext";
+
+            // Set binding context then series add
+            scs.RenderableSeries = new ObservableCollection<IRenderableSeries>(new [] { rs });
+
+            Assert.That(rs.BindingContext, Is.EqualTo("ABindingContext"));
+        }
+    }
+}
---
+++ SciChart.Xamarin.Views.Tests/Stubs/StubRenderableSeries.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SciChart.Xamarin.Views.Model.DataSeries;
+using SciChart.Xamarin.Views.Visuals.RenderableSeries;
+using Xamarin.Forms;
+
+namespace SciChart.Xamarin.Views.Tests.Stubs
+{
+    class StubRenderableSeries : IRenderableSeries
+    {
+        public Color Stroke { get; set; }
+        public int StrokeThickness { get; set; }
+        public IDataSeries DataSeries { get; set; }
+        public string YAxisId { get; set; }
+        public string XAxisId { get; set; }
+        public object BindingContext { get; set; }
+    }
+}
